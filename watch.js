05ee44f8e8ceee67507d07e7cb505446d7206cc9
Nataliia Benko <33394119+NonieBenks@users.noreply.github.com>
2018-10-31 00:28:00 +0200

    Delete watch.js

--- watch.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-    $.gulp.task('watch',function(){
-        $.gulp.watch('src/pug/**/*.pug',$.gulp.series('pug'));
-        $.gulp.watch('build/static/scss/**/*.scss',$.gulp.series('sass'))
-    })
-}
